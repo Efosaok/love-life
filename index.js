7f146ce5cc085a12747c9c95123f4cc62106afdc
Efosa Okpugie <efosaokpugie@gmail.com>
2017-03-10 23:32:59 +0100

    remodified

--- index.js
+++ index.js
@@ -19,16 +19,13 @@ app.get('/', function (req, res) {
 })
 
 // for Facebook verification
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
-});
+  app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'MY_VOICE') {
+      res.send(req.query['hub.challenge']);
+    } else {
+      res.send('Error, wrong validation token');    
+    }
+  });
 
 // Spin up the server
 app.listen(app.get('port'), function() {
