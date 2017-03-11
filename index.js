f09f87e0ffbc807c68b61778ff902b100feb2e25
Efosa Okpugie <efosaokpugie@gmail.com>
2017-03-11 01:00:44 +0100

    remodified

--- index.js
+++ index.js
@@ -27,41 +27,6 @@ app.get('/', function (req, res) {
     }
   });
 
-  app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
-        if (event.message && event.message.text) {
-            let text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-        }
-    }
-    res.sendStatus(200)
-})
-
-  const token = EAAIxZCz7lbwwBAFYRkAhYPVqKQMX8vOa1SsQy0tc2DfFrE7Im0WUcYNCZB3ZApuJE5ZBjFq7T8LVAMQSUMoZCt3XUbZAyBH2xc2N1aji5t6lKTCCPgbGcN7pG9XqD9qbU8A99ShyZCCWm5ndc0ach4JsN17CrXMZBWeQZApN8Pw8M3AZDZD
-
-
-  function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
